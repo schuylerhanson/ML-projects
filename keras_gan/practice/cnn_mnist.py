6b47cef01a777a0bf3bbb5e92a5cf9b0e949bfc6
schuylerhanson <schuyler.hanson@gmail.com>
2022-10-22 10:52:36 -0400

    (to squash): added feature_map visualization

--- keras_gan/practice/cnn_mnist.py
+++ keras_gan/practice/cnn_mnist.py
@@ -19,11 +19,10 @@ def load_img_dataset(path):
         arrs[idx] = np.asarray(Image.open(file))
     return arrs
 
-def plot_fct(x_train, y_train, fout):
+def plot_fct(x_train, y_train, fout='MNIST_data.png'):
     os.chdir('../plots')
-    fout = 'MNIST_data.png'
     fig,ax = plt.subplots(3,3,figsize=(12,12))
-    fig.suptitle('Some MNIST digits data')
+    fig.suptitle(fout.split('.')[0])
     for x in range(3):
         for y in range(3):
             ax[x,y].imshow(x_train[x+y]); ax[x,y].set_title('label = {}'.format(y_train[x+y]))
@@ -42,7 +41,9 @@ def model_1(x_data):
     return output
 
 def model_2(x_data):
-    output = keras.layers.Dense(28*28*32, activation)
+    x = keras.layers.Conv2D(filters=32, kernel_size=(5,5), activation='relu')(x_data)
+    x = keras.layers.MaxPooling2D(pool_size=(2,2))(x)
+    return x
     
 def x_data_format(x_data):
     x_data = (np.repeat(np.expand_dims(x_data, axis=-1), 3, axis=-1)).astype('float32')
@@ -66,12 +67,6 @@ y_train_tensor = y_data_format(y_train)
 x_test_tensor = x_data_format(x_test)
 y_test_tensor = y_data_format(y_test)
 
-print('X_TRAIN SHAPE:{} \n X_TRAIN TYPE:{} \n'.format(x_train_tensor.shape, type(x_train_tensor)))
-print('Y_TRAIN SHAPE', y_train_tensor.shape)
-print('X_TEST SHAPE', x_test_tensor.shape)
-print('Y_TEST SHAPE', y_test_tensor.shape)
-
-#N=25000
 N = int(x_train.shape[0])
 print('N={} TRAINING EXAMPLES \n'.format(N))
 x_train = x_train_tensor[:N]
@@ -83,22 +78,38 @@ model = keras.Model(inputs, outputs)
 
 #model.summary() 
 
-
 #%% Model action
-print('------\n MODEL TRAINING \n ------')
+'''
+print('------\n MODEL TRAINING \n------')
 
 model.compile(optimizer='adam', 
     loss='categorical_crossentropy',
     metrics=['accuracy', 'categorical_accuracy'])
 
-#print(x_train_tensor.shape, y_train_tensor.shape, x_test_tensor.shape, y_test_tensor.shape) 
-
-
 history = model.fit(x_train_tensor, y_train_tensor, 
     validation_data=(x_test_tensor, y_test_tensor),
     batch_size=128, epochs=10)
 
-print('-------\n MODEL PREDICTION \n -------')
+print('-------\n MODEL PREDICTION \n-------')
 preds = model.predict(x_test_tensor[:50])
 print('y_pred:', np.argmax(preds, axis=1), '\n')
 print('y_true:', np.argmax(y_test_tensor[:50], axis=1))
+'''
+
+#%% Visualize feature maps
+model_viz = keras.Model(inputs, outputs=model.layers[3].output)
+feature_maps = model_viz.predict(np.expand_dims(x_train_tensor[0], axis=0))
+print('feature_maps shape:', feature_maps.shape)
+
+fig,ax = plt.subplots(3,3,figsize=(20,20))
+class_label_plot = np.argmax(y_train_tensor[0])
+fig.suptitle('Feature map visualization of img of class={}'.format(class_label_plot))
+for row in range(3):
+    for col in range(3):
+        ax[row,col].imshow(feature_maps[0,:,:,row+col]); ax[row,col].set_title('feature map {}'.format(row+col))
+        ax[row,col].set_xticks([]); ax[row,col].set_yticks([])
+        ax[row,col].grid()
+fout = '/mnt/c/Users/schuy/Documents/ML-projects/plots/keras_gan/practice/feature_maps_class_{}_Conv2D_01.png'.format(class_label_plot)
+if not os.path.exists(os.path.dirname(fout)):
+    os.makedirs(os.path.dirname(fout))
+fig.savefig(fout)
